54a369bd6d4619a0c8b40a7c7b457f32ce259bf4
Laus Brandt <>
2019-08-06 20:11:09 +0200

    Added a Vault

---
+++ src/Core/Vault.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Core;
+
+
+class Vault
+{
+    static private $instance;
+    private $data = [];
+
+    static public function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function unset($name)
+    {
+        unset($this->data[$name]);
+    }
+}
