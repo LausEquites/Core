f965f32b46ad69c46cc9481bbab08541a44f937d
Thomas Faurbye Nielsen <faurbye@hey.com>
2022-08-07 13:22:37 +0000

    Remove the logging, instead just have it throw.

--- src/Core/DB.php
+++ src/Core/DB.php
@@ -37,21 +37,12 @@ class DB
         $dsn      = "mysql:dbname={$dbConf->db};host={$dbConf->host};charset=utf8mb4";
         $user     = $dbConf->user;
         $password = $dbConf->pass;
-        try {
-            return new \PDO(
-                $dsn, $user, $password,
-                [
-                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                ]
-            );
-        } catch (\Throwable $exception) {
-            if (\APP_ENV !== 'production') {
-                var_dump(['dsn' => $dsn, 'user' => $user,]);
-            }
-            if(\APP_ENV === 'test') {
-                codecept_debug(['dsn' => $dsn, 'user' => $user,]);
-            }
-            throw $exception;
-        }
+       
+        return new \PDO(
+            $dsn, $user, $password,
+            [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            ]
+        );
     }
 }
