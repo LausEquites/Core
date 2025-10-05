b265afd045b17ed98d15f8ea028969412d3f48ef
Laus Brandt <laus@equites.dk>
2025-10-05 23:20:58 +0200

    Added json params to openAPI

--- example/index.php
+++ example/index.php
@@ -7,6 +7,7 @@ header('Access-Control-Allow-Headers: *');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(204); exit; }
 
 ini_set('display_errors', 1);
+ini_set('html_errors', 0);
 
 include_once __DIR__ . "/../vendor/autoload.php";
 include "Controllers/Foo.php";
